c76f51ff5ab7894eb337db0c10cccfec8cabc0a4
stephantul <stephantul@gmail.com>
2025-08-06 13:44:34 +0200

    feat: new helpers

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,16 @@ import json
 from typing import Any
 
 from pytest import fixture
+from tokenizers import Tokenizer
 
 
 @fixture(scope="function")
 def small_tokenizer_json() -> dict[str, Any]:
     """Load an extremely small tokenizer for testing purposes."""
     return json.load(open("tests/data/small_tokenizer.json", "r", encoding="utf-8"))
+
+
+@fixture(scope="module")
+def small_tokenizer() -> Tokenizer:
+    """Load an extremely small tokenizer for testing purposes."""
+    return Tokenizer.from_file("tests/data/small_tokenizer.json")
