7c2daf367140166a28956fd3bb2828dbfcae2127
stephantul <stephantul@gmail.com>
2025-09-01 10:07:17 +0200

    Turn default padding strategy in Fixed to facilitate storage of padding token

--- skeletoken/padding.py
+++ skeletoken/padding.py
@@ -1,11 +1,23 @@
 from enum import Enum
+from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 
 
-class PaddingStrategy(str, Enum):
-    BATCH_LONGEST = "BatchLongest"
-    FIXED = "Fixed"
+class BatchLongestStrategy(RootModel[Literal["BatchLongest"]]):
+    """Pad to the longest string in the sequence."""
+
+    ...
+
+
+class FixedStrategy(BaseModel):
+    """Pad to a fixed amount."""
+
+    Fixed: int
+
+
+# Union of the two shapes
+PaddingStrategy = BatchLongestStrategy | FixedStrategy
 
 
 class PaddingDirection(str, Enum):
@@ -16,7 +28,7 @@ class PaddingDirection(str, Enum):
 class Padding(BaseModel):
     """A padding configuration."""
 
-    strategy: PaddingStrategy = PaddingStrategy.BATCH_LONGEST
+    strategy: PaddingStrategy = FixedStrategy(Fixed=0)
     direction: PaddingDirection = PaddingDirection.RIGHT
     pad_to_multiple_of: int | None = None
     pad_id: int
