3bad71a674a1849363e6a9ea9eed7b0ab09c9e38
stephantul <stephantul@gmail.com>
2025-09-17 10:52:27 +0200

    fix: tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from typing import Any
 
 from pytest import FixtureRequest, fixture
@@ -8,18 +9,28 @@ from transformers import PreTrainedTokenizerFast
 from skeletoken.merges import Merges
 
 
+def _get_path(name: str) -> Path:
+    """Gets the path to a test resource."""
+    return Path("tests") / "data" / name
+
+
+def _get_tokenizers_path(name: str) -> str:
+    """Gets the path to a test tokenizer resource."""
+    return str(_get_path(name) / "tokenizer.json")
+
+
 @fixture(scope="function")
 def small_tokenizer_json(request: FixtureRequest) -> dict[str, Any]:
     """Load an extremely small tokenizer for testing purposes."""
-    name = getattr(request, "param", None) or "bpe"
-    return json.load(open(f"tests/data/{name}/tokenizer.json", "r", encoding="utf-8"))
+    name = getattr(request, "param", None) or "wordpiece"
+    return json.load(open(_get_tokenizers_path(name), "r", encoding="utf-8"))
 
 
 @fixture(scope="module")
 def small_tokenizer(request: FixtureRequest) -> Tokenizer:
     """Load an extremely small tokenizer for testing purposes."""
-    name = getattr(request, "param", None) or "bpe"
-    return Tokenizer.from_file(f"tests/data/{name}/tokenizer.json")
+    name = getattr(request, "param", None) or "wordpiece"
+    return Tokenizer.from_file(_get_tokenizers_path(name))
 
 
 @fixture(scope="function")
@@ -31,6 +42,6 @@ def small_merges() -> Merges:
 @fixture(scope="function")
 def transformers_tokenizer(request: FixtureRequest) -> PreTrainedTokenizerFast:
     """Load a small transformers tokenizer for testing purposes."""
-    name = getattr(request, "param", None) or "bpe"
-    tokenizer = PreTrainedTokenizerFast.from_pretrained(f"tests/data/{name}")
+    name = getattr(request, "param", None) or "wordpiece"
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(_get_path(name))
     return tokenizer
